b87aef2b3d95d29a3925c593effa14d25962f5f4
phifertoo <phiferthree@gmail.com>
2020-05-11 22:13:00 -0700

    2nd commit

--- index.js
+++ index.js
@@ -20,14 +20,22 @@ const wsServer = new webSocketServer({
   httpServer: httpServer,
 });
 
+//resolving requests from the client
 wsServer.on("request", (request) => {
-  //connecting from the client
+  //logic to make a connection to the client
   const connection = request.accept(null, request.origin);
   connection.on("open", () => console.log("opened!"));
   connection.on("close", () => console.log("closed!"));
+  /* mapping the function that triggers when the server
+   receives a request */
   connection.on("message", (message) => {
-    //data that the server receives which is coming as a JSON object
+    /*parse the data that the server receives which is 
+ coming as a JSON object. The data is stored in "result" */
     const result = JSON.parse(message.utf8Data);
+    /* when the server receives a create request, the server will
+    create a gameId. Based on this gameId, the server stores the game
+    as a key in the "games" object with the gameId, 20 balls, 
+    and clients  */
     if (result.method === "create") {
       const clientId = result.clientId;
       const gameId = guid();
@@ -37,11 +45,13 @@ wsServer.on("request", (request) => {
         clients: [],
       };
 
-      //send payload to client
+      /* the server will send a payload that includes the data in the 
+      game (id, balls, clients) see above*/
       const payLoad = {
         method: "create",
         game: games[gameId],
       };
+      //set up connection to a specific clientId
       const con = clients[clientId].connection;
       //JSON.stringify when sending data. JSON.parse when receiving data
       con.send(JSON.stringify(payLoad));
@@ -51,22 +61,29 @@ wsServer.on("request", (request) => {
       const clientId = result.clientId;
       const gameId = result.gameId;
       let game = games[gameId];
+      //once the game has 3 clients/players, don't do anything
       if (game.clients.length >= 3) {
         return;
       }
       /*if game.clients.length ===0, it will return "Red"
-      If game.clients.length === 1, it will return "Green"*/
+      If game.clients.length === 1, it will return "Green."
+      This will set the player's color. */
       const color = { 0: "Red", "1": "Green", "2": "Blue" }[
         game.clients.length
       ];
+      /* add this player to the game.clients array. Each player
+      has a clientId and color*/
       game.clients.push({
         clientId,
         color,
       });
-      //when there are 3 clients, start the game.
+      /*when there are 3 clients, start the game. After the game
+    has started, the server will call updateGameState every time
+    it receives a request. */
       if (game.clients.length === 3) {
         updateGameState();
       }
+      //the payload includes the game object
       const payLoad = {
         method: "join",
         game,
@@ -76,15 +93,18 @@ wsServer.on("request", (request) => {
         clients[c.clientId].connection.send(JSON.stringify(payLoad));
       });
     }
+    /* if the player clicks on a square ("play" request),
+    the server receives the gameId, ballId, and player's color*/
     if (result.method === "play") {
-      const clientId = result.clientId;
       const gameId = result.gameId;
       const ballId = result.ballId;
       const color = result.color;
-      let state = games[gameId].state;
-      if (!state) {
-        state = {};
-      }
+      /* create an object called state that stores the colors
+      of each ball based on the ballId and the player's color
+      sent from the client. Stores the state in the games object
+      so when you send the games object back to the client, it will
+      contain the state */
+      let state = {};
       state[ballId] = color;
       games[gameId].state = state;
     }
@@ -93,15 +113,19 @@ wsServer.on("request", (request) => {
   //generate a new clientId
   const clientId = guid();
   clients[clientId] = {
-    connection: connection,
+    connection,
   };
 
-  //send response to client
+  /*upon the initial connection between the server and client,
+  the server will send the client a clientId. */
   const payLoad = {
     method: "connect",
-    clientId: clientId,
+    clientId,
   };
 
+  /* This only fires if none of the other request types
+  are received. 
+  */
   connection.send(JSON.stringify(payLoad));
 });
 
